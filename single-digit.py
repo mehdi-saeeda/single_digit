f96268c19c5c11473ca72e53d1e62ed3733b48ac
mehdi-saeeda <mehdisaeedaa@gmail.com>
2024-10-31 15:49:30 +0330

    completed code

--- single-digit.py
+++ single-digit.py
@@ -10,3 +10,7 @@ def single_digit():
             print("Please enter a valid number.")
     while number >= 10:
         number = sum(int(digit) for digit in str(number))
+
+    print(f"The single digit is: {number}")
+
+single_digit()
