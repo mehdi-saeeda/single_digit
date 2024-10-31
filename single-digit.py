0dcee9bd922c66db4b27118e28ef26aa927f8417
mehdi-saeeda <mehdisaeedaa@gmail.com>
2024-10-31 15:57:13 +0330

    improve code

--- single-digit.py
+++ single-digit.py
@@ -5,9 +5,14 @@ def single_digit():
             if number < 0:
                 print("Please enter a positive number.")
                 continue
+            if number < 10:
+                print(f"The number is already a single digit: {number}. Exiting the program.")
+                return
+
             break
         except ValueError:
             print("Please enter a valid number.")
+
     while number >= 10:
         number = sum(int(digit) for digit in str(number))
 
